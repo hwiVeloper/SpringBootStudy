97fd53c86e9ffc15a7976d09daae0272959745bb
Jonghwi Lee <zziller03@gmail.com>
2018-07-08 18:40:14 +0900

    스프링부트 & mysql with JPA

---
+++ boot02/src/main/java/org/hwi/Boot02Application.java
@@ -0,0 +1,12 @@
+package org.hwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot02Application.class, args);
+	}
+}
