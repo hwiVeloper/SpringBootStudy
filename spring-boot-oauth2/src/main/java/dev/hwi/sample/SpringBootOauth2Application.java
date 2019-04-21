495193fb5ca875be9b7833765ce6379971bc0ba0
Jonghwi Lee <zziller03@gmail.com>
2019-04-21 18:07:06 +0900

    20190421 Spring boot oauth2 auth server

--- spring-boot-oauth2/src/main/java/dev/hwi/sample/SpringBootOauth2Application.java
+++ spring-boot-oauth2/src/main/java/dev/hwi/sample/SpringBootOauth2Application.java
@@ -1,15 +1,16 @@
 package dev.hwi.sample;
 
+import java.security.Principal;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.Filter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -24,7 +25,6 @@ import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
@@ -33,15 +33,25 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.filter.CompositeFilter;
 
 @SpringBootApplication
 @EnableOAuth2Client
 @EnableAuthorizationServer
+@RestController
 public class SpringBootOauth2Application extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	OAuth2ClientContext oauth2ClientContext;
+	
+	@RequestMapping({ "/user", "/me" })
+	public Map<String, String> user(Principal principal) {
+		Map<String, String> map = new LinkedHashMap<>();
+		map.put("name", principal.getName());
+		return map;
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootOauth2Application.class, args);
@@ -51,10 +61,10 @@ public class SpringBootOauth2Application extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.antMatcher("/**")
 			.authorizeRequests()
-			.antMatchers("/", "/login**", "/webjars/**", "/error**").permitAll()
+			.antMatchers("/", "/login**", "/webjars/**").permitAll()
 			.anyRequest().authenticated()
 			.and().exceptionHandling()
-				.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
+				  .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
 			.and().logout().logoutSuccessUrl("/").permitAll()
 			.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 			.and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
@@ -92,30 +102,6 @@ public class SpringBootOauth2Application extends WebSecurityConfigurerAdapter{
 		filter.setTokenServices(tokenServices);
 		return filter;
 	}
-
-//	@Bean
-//	@ConfigurationProperties("facebook.client")
-//	public OAuth2ProtectedResourceDetails facebook() {
-//		return new AuthorizationCodeResourceDetails();
-//	}
-//	
-//	@Bean
-//	@ConfigurationProperties("facebook.resource")
-//	public ResourceServerProperties facebookResource() {
-//		return new ResourceServerProperties();
-//	}
-//	
-//	@Bean
-//	@ConfigurationProperties("github.client")
-//	public OAuth2ProtectedResourceDetails github() {
-//		return new AuthorizationCodeResourceDetails();
-//	}
-//
-//	@Bean
-//	@ConfigurationProperties("github.resource")
-//	public ResourceServerProperties githubResource() {
-//		return new ResourceServerProperties();
-//	}
 	
 	@Bean
 	@ConfigurationProperties("github")
