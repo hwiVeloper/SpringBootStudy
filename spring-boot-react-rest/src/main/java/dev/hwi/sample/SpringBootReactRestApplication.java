77e6ebc265f34fcf96cf6a5ccbd3b0871a616fc7
Jonghwi Lee <zziller03@gmail.com>
2019-04-30 17:54:14 +0900

    20190430 spring-boot-react-rest

---
+++ spring-boot-react-rest/src/main/java/dev/hwi/sample/SpringBootReactRestApplication.java
@@ -0,0 +1,13 @@
+package dev.hwi.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactRestApplication.class, args);
+	}
+
+}
