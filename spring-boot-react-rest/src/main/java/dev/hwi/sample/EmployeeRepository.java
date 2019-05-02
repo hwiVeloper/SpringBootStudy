cb31504d22e9269d55b5c617a1b8c77df9753418
Jonghwi Lee <zziller03@gmail.com>
2019-05-02 17:46:47 +0900

    20190502 react 페이징 조회

--- spring-boot-react-rest/src/main/java/dev/hwi/sample/EmployeeRepository.java
+++ spring-boot-react-rest/src/main/java/dev/hwi/sample/EmployeeRepository.java
@@ -1,7 +1,7 @@
 package dev.hwi.sample;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long>{
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long>{
 
 }
